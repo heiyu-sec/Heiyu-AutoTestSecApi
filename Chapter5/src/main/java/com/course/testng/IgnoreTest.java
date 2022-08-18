77704fabed6a4cfdb82a9371f3111e1d14c3932d
heiyu-sec <304429477@qq.com>
2022-08-18 15:14:21 +0800

    忽略测试case

---
+++ Chapter5/src/main/java/com/course/testng/IgnoreTest.java
@@ -0,0 +1,18 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class IgnoreTest {
+    @Test
+    public void ignore1(){
+        System.out.println("ignore1 执行");
+    }
+    @Test(enabled = false)
+    public void ignore2(){
+        System.out.println("ignore2执行");
+    }
+    @Test(enabled = true)
+    public void ignore3(){
+        System.out.println("ignore3执行");
+    }
+}
