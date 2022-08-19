6713446dff202b4ba96ed5fe895bd89c5ad30129
heiyu-sec <304429477@qq.com>
2022-08-19 12:08:26 +0800

    组测试中的方法分组测试

---
+++ Chapter5/src/main/java/com/course/testng/groups/GroupsOnMethod.java
@@ -0,0 +1,41 @@
+package com.course.testng.groups;
+
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GroupsOnMethod {
+    @Test(groups = "server")
+    public void test1(){
+        System.out.println("这是服务端组的测试方法1111");
+    }
+    @Test(groups = "server")
+    public void test2(){
+        System.out.println("这是服务端组的测试方法22222");
+    }
+    @Test(groups = "client")
+    public void test3() {
+        System.out.println("这是客户端组的测试方法33333");
+    }
+    @Test(groups = "client")
+    public void test4() {
+        System.out.println("这是客户端组的测试方法444444");
+    }
+    @BeforeGroups("server")
+    public void beforeGroupsOnServer(){
+        System.out.println("这是服务端组运行之前运行的方法！！！！！！");
+    }
+    @AfterGroups("server")
+    public void afterGroupsOnServer(){
+        System.out.println("这是服务端组运行后运行的方法！！！！");
+    }
+    @BeforeGroups("client")
+    public void beforeGroupsOnClient(){
+        System.out.println("这是客户端组运行之前运行的方法！！！！！！");
+    }
+    @AfterGroups("client")
+    public void afterGroupsOnClient(){
+        System.out.println("这是客户端组运行后运行的方法！！！！");
+    }
+}
