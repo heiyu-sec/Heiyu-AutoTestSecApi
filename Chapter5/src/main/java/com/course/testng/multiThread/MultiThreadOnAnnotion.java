618a1111199bb8f912485a3bb7f6753355ef38a5
heiyu-sec <304429477@qq.com>
2022-08-24 14:01:33 +0800

    多线程测试-注解方式实现

---
+++ Chapter5/src/main/java/com/course/testng/multiThread/MultiThreadOnAnnotion.java
@@ -0,0 +1,11 @@
+package com.course.testng.multiThread;
+
+import org.testng.annotations.Test;
+
+public class MultiThreadOnAnnotion {
+    @Test(invocationCount = 10,threadPoolSize = 3)
+    public void test(){
+        System.out.println(1);
+        System.out.printf("Threadid:%s %n",Thread.currentThread().getId());
+    }
+}
