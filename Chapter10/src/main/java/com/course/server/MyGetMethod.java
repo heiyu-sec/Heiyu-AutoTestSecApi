a1eda85bfdb561ac57f086ec7ec6f7125fc7356b
heiyu-sec <304429477@qq.com>
2022-08-31 15:56:22 +0800

    携带cookies信息访问的get接口开发

--- Chapter10/src/main/java/com/course/server/MyGetMethod.java
+++ Chapter10/src/main/java/com/course/server/MyGetMethod.java
@@ -4,7 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @RestController
 public class MyGetMethod {
@@ -15,5 +17,23 @@ public class MyGetMethod {
         Cookie cookie = new Cookie("login","true");
         response.addCookie(cookie);
         return "恭喜你获得cookies信息成功";
+    }
+
+    /**
+     * 要求客户端携带cookies访问
+     * 这是一个需要携带cookies信息才能访问的get请求
+     */
+    @RequestMapping(value = "/get/with/cookies",method = RequestMethod.GET)
+    public String getWithCookies(HttpServletRequest request){
+        Cookie[] cookies=request.getCookies();
+        if(Objects.isNull(cookies)){
+            return "你必须携带cookies信息来";
+        }
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("login")&&cookie.getValue().equals("true")){
+                return "恭喜你访问成功！";
+            }
+        }
+        return "你必须携带cookies信息来";
     }
 }
