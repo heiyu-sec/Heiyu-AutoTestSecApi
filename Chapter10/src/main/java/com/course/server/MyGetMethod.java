0b39060df67e83d92d97b1b56afe190fc97e2fe5
heiyu-sec <304429477@qq.com>
2022-08-31 10:58:10 +0800

    返回cookies信息的get接口开发

---
+++ Chapter10/src/main/java/com/course/server/MyGetMethod.java
@@ -0,0 +1,19 @@
+package com.course.server;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class MyGetMethod {
+    @RequestMapping(value = "/getCookies",method = RequestMethod.GET)
+    public String getCookies(HttpServletResponse response){
+        //HttpServerletRequest 装请求信息的类
+        //HttpServerletResponse  装响应信息的类
+        Cookie cookie = new Cookie("login","true");
+        response.addCookie(cookie);
+        return "恭喜你获得cookies信息成功";
+    }
+}
